a9fd57acbc9a57db94b75fea16a58f3b9406eb31
AsherChen <gpks556@gmail.com>
2023-01-20 19:30:38 +0800

    Asher Chen commited at 2023-01-20_19-30-38

--- src/svg.js
+++ src/svg.js
@@ -1,7 +1,10 @@
 import { SVG } from '@svgdotjs/svg.js'
-
-SVG()
-  .addTo('#svg')
-  .rect(100, 100)
-  .move(100, 50)
-  .fill('#f06')
+var draw = SVG().addTo('body').size(300, 300)
+var rect = draw.rect(100, 100).animate({
+  duration: 2000,
+  delay: 1000,
+  when: 'now',
+  swing: true,
+  times: 5,
+  wait: 200
+}).attr({ fill: '#f06' })
